25d1bfd567dd6f0190250eca44cebce3f6636223
jcraiz <63567373+jcraiz@users.noreply.github.com>
2022-05-10 02:39:54 -0500

    Add files via upload

---
+++ data/slide3.js
@@ -0,0 +1,5 @@
+(function(){var loadHandler=window['sl_{1F8FDDC7-C960-4661-9054-B42EBC7AAF27}'];loadHandler&&loadHandler(2, '<div id="spr0_d0b794"><div id="spr1_d0b794" class="kern slide"><img id="img2_d0b794" src="data/img2.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_d0b794" class="kern slide"><div id="spr3_d0b794" style="top:503.993px;"><img id="img0_d0b794" src="data/img0.png" width="507.75" height="192" alt="Logotipo\
+\
+Descripción generada automáticamente con confianza baja" style="left:0.029px;top:-0.2px;"/></div><div id="spr4_d0b794" style="left:411.7px;top:185.988px;"><img id="img1_d0b794" src="data/img8.png" width="454.5" height="427.5" alt="Icono\
+\
+Descripción generada automáticamente" style="left:0.162px;top:-0.088px;"/></div><div id="spr5_d0b794" style="left:281.739px;top:82.087px;"><div style="width:0px;"><span id="txt0_d0b794" data-width="575.479187" style="left:71.857px;top:0.978px;">This is the end of the activity!</span></div><div style="width:0px;"><span id="txt1_d0b794" data-width="575.479187" style="left:70.261px;top:-0.619px;">This is the end of the activity!</span></div></div></div></div>', '{"s":[]}');})();
